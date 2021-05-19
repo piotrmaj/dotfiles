4d06a50812176ab8bb142d588914842c9b975cc6
Piotr Maj <piotrmaj17@gmail.com>
2021-05-19 20:53:07 +0200

    fix symlink for libinput-gestures

--- symlink.sh
+++ symlink.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/git/.gitconfig ~
-ln -s ~/dotfiles/libinput-gestures.conf ~/.config/
+ln -s ~/dotfiles/libinput-gestures.conf ~/.config/libinput-gestures.conf
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 

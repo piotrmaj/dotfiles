e788f21540f2a3d08cb0f4e37ed0dc2dec2ef397
Piotr Maj <piotrmaj17@gmail.com>
2022-07-30 12:29:44 +0200

    Fix symlink so it can be executed from any directory

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,9 @@
-#ln -s ~/dotfiles/vim/.vimrc ~
+#ln -s $SCRIPT_DIR/vim/.vimrc ~
 mkdir -p ~/.config/nvim
-ln -s ~/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
-ln -s ~/dotfiles/git/.gitconfig ~
-ln -s ~/dotfiles/libinput-gestures.conf ~/.config/libinput-gestures.conf
-ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ln -f -s $SCRIPT_DIR/vim/.vimrc ~/.config/nvim/init.vim
+ln -f -s $SCRIPT_DIR/git/.gitconfig ~
+ln -f -s $SCRIPT_DIR/libinput-gestures.conf ~/.config/libinput-gestures.conf
+ln -f -s $SCRIPT_DIR/zsh/.zshrc ~/.zshrc
+ln -f -s $SCRIPT_DIR/zsh/.p10k.zsh ~/.p10k.zsh
 

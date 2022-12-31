b5933d40c7112be3785d8e9d659acca0c5f1e4b9
Piotr Maj <piotrmaj17@gmail.com>
2022-12-31 15:07:47 +0100

    Finally everything seems to be working inside docker

--- run_test_docker.sh
+++ run_test_docker.sh
@@ -1,5 +1,6 @@
 docker run --rm -it \
   --mount type=bind,source=/home/maju/.ssh/id_rsa,target=/root/.ssh/id_rsa,readonly \
-  --mount type=bind,source=/home/maju/dotfiles,target=/root/dotfiles \
-  ubuntu bash
+  --mount type=bind,source=/home/maju/dotfiles,target=/dotfiles \
+  -e "TERM=xterm-256color" \
+  ubuntu bash -c "apt-get update && apt-get install curl sudo -y && cd /dotfiles && bash"
 

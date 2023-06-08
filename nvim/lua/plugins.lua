be22337b0c73758afe5bd593e1f6a65948ca66cc
Piotr Maj <piotrmaj17@gmail.com>
2023-06-08 11:16:40 +0200

    Migrate to nvim lua

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,8 @@
+return {
+
+        --	LSP
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig", -- enable LSP
+	"williamboman/nvim-lsp-installer",
+}
--- symlink.sh
+++ symlink.sh
@@ -1,11 +1,11 @@
 #ln -s $SCRIPT_DIR/vim/.vimrc ~
-mkdir -p ~/.config/nvim
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ln -f -s $SCRIPT_DIR/vim/.vimrc ~/.config/nvim/init.vim
+#mkdir -p ~/.config/nvim
+#ln -f -s $SCRIPT_DIR/vim/.vimrc ~/.config/nvim/init.vim
+ln -f -s $SCRIPT_DIR/nvim ~/.config
 ln -f -s $SCRIPT_DIR/git/.gitconfig ~
 ln -f -s $SCRIPT_DIR/libinput-gestures.conf ~/.config/libinput-gestures.conf
 ln -f -s $SCRIPT_DIR/zsh/.zshrc ~/.zshrc
 ln -f -s $SCRIPT_DIR/zsh/.p10k.zsh ~/.p10k.zsh
 ln -f -s $SCRIPT_DIR/i3/config ~/.config/i3/config
 ln -f -s $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
-
